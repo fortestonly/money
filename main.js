8ed9fb3c7717f727efaf50fa004bb9a546b5a388
Evgeny Vlasenko <mahnunchik@gmail.com>
2020-06-03 14:13:50 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,81 @@
+'use strict';
+
+// Modules to control application life and create native browser window
+const { app, shell, BrowserWindow, Menu } = require('electron');
+const path = require('path');
+
+app.allowRendererProcessReuse = true;
+
+const isMac = process.platform === 'darwin';
+
+const helpMenu = {
+  role: 'help',
+  submenu: [
+    {
+      label: 'Learn More',
+      click: async () => {
+        await shell.openExternal('https://electronjs.org');
+      },
+    },
+    ...(!isMac ? [
+      { type: 'separator' },
+      {
+        role: 'about',
+      },
+    ] : []),
+  ],
+};
+
+const menuTemplate = [
+  ...(isMac ? [{ role: 'appMenu' }] : []),
+  { role: 'fileMenu' },
+  { role: 'editMenu' },
+  { role: 'viewMenu' },
+  { role: 'windowMenu' },
+  helpMenu,
+];
+
+const menu = Menu.buildFromTemplate(menuTemplate);
+Menu.setApplicationMenu(menu);
+
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
---
+++ renderer.js
@@ -0,0 +1,15 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// No Node.js APIs are available in this process because
+// `nodeIntegration` is turned off. Use `preload.js` to
+// selectively enable features needed in the rendering
+// process.
+
+const app = require('electron').remote.app;
+
+const replaceText = (selector, text) => {
+  const element = document.getElementById(selector)
+  if (element) element.innerText = text
+}
+
+replaceText('app-version', app.getVersion())
